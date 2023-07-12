c6cae7385a26ce9f5c94739ed979bc9f8bfe38f9
max9996661 <kazantsevMAgenius@yandex.ru>
2023-07-13 00:02:02 +0300

    Initial Commit

---
+++ ImageFeedRes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeedRes
+//
+//  Created by Максим Казанцев on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
